c8ed5483f43be3a730421532105ea144454b1764
= <=>
2020-10-31 15:54:37 +0400

    Add displaying todos functionality

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,9 +2,18 @@ import React from "react";
 import "./TodoList.css";
 
 import Todo from "../Todo/Todo";
+import { useSelector } from "react-redux";
 
 const TodoList = () => {
-  return <div className="todoList">{/* todoContainer.map(todo => todo)*/}</div>;
+  const todoContainer = useSelector(state => state);
+
+  return (
+    <div className="todoList">
+      {todoContainer.map(todo => (
+        <Todo key={todo.id} id={todo.id} title={todo.title} description={todo.description} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
